59f49a7bc8fc7f1ee339ea015128f4be7cb8ee70
GSP <shankarg.kvsulur@gmail.com>
2022-12-15 23:26:14 +0530

    @ 2

--- Advanced/Sorting/MergeSort.js
+++ Advanced/Sorting/MergeSort.js
@@ -32,3 +32,51 @@ function (A, B) {
     // console.log('C', C)
     return C;
 }
+
+
+
+
+function mergeSort(array) {
+  // Write your code here.
+
+  if(array.length <= 1) return array;
+  const midIndex = Math.floor(array.length/2)
+  const left = array.slice(0, midIndex)
+  const right = array.slice(midIndex)
+  return test(mergeSort(left), mergeSort(right))
+  
+}
+
+
+function test(A, B){
+   let length = A.length + B.length;
+        let mergeArray = []
+        let c = 0;
+        let i = 0;
+        let j = 0;
+        while (i < A.length && j < B.length) {
+            if (A[i] < B[j]) {
+                mergeArray[c] = A[i];
+                c++;
+                i++;
+            }
+            else {
+                mergeArray[c] = B[j];
+                c++;
+                j++;
+            }
+        }
+        while (i < A.length) {
+            mergeArray[c] = A[i];
+            c++;
+            i++;
+        }
+        while (j < B.length) {
+            mergeArray[c] = B[j];
+            c++;
+            j++;
+        }
+        return mergeArray;
+}
+// Do not edit the line below.
+exports.mergeSort = mergeSort;
