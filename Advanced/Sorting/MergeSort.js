22ed44bc6c11eea084fb9db5ee292769d815c74c
GSP <Gowri.Shankar@tecnotree.com>
2022-12-16 22:47:20 +0530

    Test 2

--- Advanced/Sorting/MergeSort.js
+++ Advanced/Sorting/MergeSort.js
@@ -80,3 +80,30 @@ function test(A, B){
 }
 // Do not edit the line below.
 exports.mergeSort = mergeSort;
+
+
+
+const merge=(left,right)=>{
+    let res=[],l=0,r=0,i=0;
+
+    while(l<left.length && r<right.length){
+        if(left[l] <= right[r])res[i++]=left[l++];
+        else res[i++]=right[r++];
+    }
+
+    return res.concat(left.slice(l),right.slice(r));
+}
+
+const mergeSort = (nums,l,r)=>{
+    if(l===r-1)return [nums[l]];
+
+    let mid=(l+r)/2|0;
+
+    let left=mergeSort(nums,l,mid);
+    let right=mergeSort(nums,mid,r);
+
+    return merge(left,right);
+}
+var sortArray = function(nums) {
+    return mergeSort(nums,0,nums.length);
+};
