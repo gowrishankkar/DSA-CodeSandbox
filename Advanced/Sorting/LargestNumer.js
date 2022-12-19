6bcf5258fceb55ad64b34f2656318745ce56d30e
GSP <Gowri.Shankar@tecnotree.com>
2022-12-19 23:36:31 +0530

    Largest number

---
+++ Advanced/Sorting/LargestNumer.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    let A = nums
+     let str = ""
+        A = A.sort((a, b) => {
+            let n1 = Number('' + a + '' + b);
+            let n2 = Number('' + b + '' + a);
+            if (n1 > n2) {
+                return -1
+            }
+            else return 1
+        })
+        let zero = 0
+        for (let i = 0; i < A.length; i++) {
+            if (A[i] == 0) {
+                zero++ //to handle array case of all zeros [0,0,0,0]
+            }
+            str += A[i]
+        }
+        return zero == A.length ? "0" : str
+};
