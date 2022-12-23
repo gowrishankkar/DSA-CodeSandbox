09a417fa1e3dd06a5446252e317497b8372cf305
GSP <Gowri.Shankar@tecnotree.com>
2022-12-23 22:50:03 +0530

    Contains Duplicate

--- Advanced/Sorting/MaxMod.js
+++ Advanced/Sorting/MaxMod.js
@@ -1,10 +1,9 @@
-function  test(A) {
-    A = [...new Set(A)];
-    // sort the array
-    A.sort((a, b) => a - b);
-    // If all the elements are same then no matter which indices we choose, the answer will always be 0
-    if (A.length <= 1)
-        return 0;
-    // Otherwise the answer is the second maximum value of the set
-    return A[A.length - 2];
+function test(A) {
+  A = [...new Set(A)];
+  // sort the array
+  A.sort((a, b) => a - b);
+  // If all the elements are same then no matter which indices we choose, the answer will always be 0
+  if (A.length <= 1) return 0;
+  // Otherwise the answer is the second maximum value of the set
+  return A[A.length - 2];
 }
---
+++ Advanced/Sorting/Unique Numbers.js
@@ -0,0 +1,13 @@
+function test (A) {
+    A.sort((a, b) => a - b);
+    let count = 0;
+    for (let i = 1; i < A.length; i++) {
+        if (A[i - 1] < A[i]) {
+            continue;
+        } else {
+            count += A[i - 1] + 1 - A[i];
+            A[i] += A[i - 1] + 1 - A[i];
+        }
+    }
+    return count;
+}
